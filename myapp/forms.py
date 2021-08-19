f25e062e6f859e6a7b19193a9074c82c9ee734e2
faiz4work <faizahmed11234@gmail.com>
2021-08-19 21:04:14 +0500

    adding new files

--- myapp/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired
-
-
-class LoginForm(FlaskForm):
-    ipaddress = StringField('ipaddress', validators=[InputRequired()])
-    password = PasswordField('password', validators=[InputRequired()])
